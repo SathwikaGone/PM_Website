001786a29d0ce4f5ffcb08398d9210b70eb33bff
SathwikaGone <gonesathwika@gmail.com>
2020-01-05 23:46:11 -0600

    committing CRUD operations for employees

---
+++ CURD/Migrations/20200103230757_Match.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CURD.Migrations
+{
+    public partial class Match : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmpCode",
+                table: "Employees",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Matchs",
+                columns: table => new
+                {
+                    MatchId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    KnowledgeArea = table.Column<string>(type: "nvarchar(250)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matchs", x => x.MatchId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Matchs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmpCode",
+                table: "Employees",
+                type: "varchar(10)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+        }
+    }
+}
